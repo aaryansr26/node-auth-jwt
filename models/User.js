ba651f8b8422f5d59010ff566c60d7e83a3ca371
aaryansr26 <aaryansr26@gmail.com>
2023-03-07 21:56:36 +0530

    Mongoose validation and custom error handling

--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator'); 
 
 const userSchema = mongoose.Schema({
     "email": {
         type: String, 
-        required: true, 
+        required: [true, 'Please enter a email'], 
         unique: true, 
         lower_case: true, 
+        validate: [isEmail, 'Please enter a valid email']
     }, 
 
     "password": {
         type: String, 
-        required: true, 
-        minLength: 6
+        required: [true, 'Please enter a password'], 
+        minlength: [6, 'Minimum Password length is 6 characters!'] 
     }
 }) ;
 
