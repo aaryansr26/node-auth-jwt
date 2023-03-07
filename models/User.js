123322795471e35e950474947af66064a8d7c0a4
aaryansr26 <aaryansr26@gmail.com>
2023-03-07 17:32:47 +0530

    added models for mongodb

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    "email": {
+        type: String, 
+        required: true, 
+        unique: true, 
+        lower_case: true, 
+    }, 
+
+    "password": {
+        type: String, 
+        required: true, 
+        minLength: 6
+    }
+}) ;
+
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User; 
+
