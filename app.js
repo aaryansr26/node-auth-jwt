09fab6e67c8f05127e6b3ec83cc91af259edc810
aaryansr26 <aaryansr26@gmail.com>
2023-03-15 19:41:43 +0530

    protected smoothies route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { Mongoose } = require('mongoose');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser'); 
+const { requireAuth } = require('./middleware/authMiddleware');
 
 
 const app = express();
@@ -23,5 +24,5 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes); 
